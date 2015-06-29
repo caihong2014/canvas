2b8026157764ca641e2fa7b310d81626d4943750
Pearyman <liyananhappy@sina.cn>
2015-06-29 20:06:33 +0800

    get data

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 var http=require('http');
 http.createServer(function(req,res){
 	res.writeHead(200,{'Content-Type':'text/html'});
-	res.write('<h1>hello world</h1>');
+	// res.write('<h1>hello world</h1>');
 	res.end();
 }).listen(8081);
 console.log('http server is listening the port 8081');
--- getlaugh.js
+++ getlaugh.js
@@ -12,12 +12,12 @@ var AV = require('avoscloud-sdk').AV;
 AV.initialize("a5zjlnxgv6vhjnstba351wh97s3tc40hsot0no9j2b9wa153", "qhod8u5iijtvgm16g07gw1dm8f4mgmtqnthsloc7rqkyoxgb");
 
 var opts = {
-	url: 'http://daily.zhihu.com/story/4817486',
+	url: 'http://heibaimanhua.com/funlailiao/67216.html',
 	header: {
 		'Connection': 'keep-alive',
 		'Accept-Language':'zh-CN,zh;q=0.8',
 		'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36',
-		'Referer': 'http://www.zhihu.com'
+		'Referer': 'http://heibaimanhua.com'
 
 	}
 }
@@ -27,7 +27,7 @@ request(opts, function(error, response, body) {
 			var $ = cheerio.load(body);
 				var TestObject = AV.Object.extend("dailyNews");
 				var testObject = new TestObject();
-				testObject.save({question:$(".headline-title").text(),img_url:$(".img-wrap img").attr("src"),content:$('.content').html(),title:$(".headline-title").text(),big_img:$(".img-wrap img").attr("src"),url_href:opts.url}, {
+				testObject.save({question:$(".article-header").text(),img_url:'http://ac-a5zjlnxg.clouddn.com/9bVyIixddpREs3SgJcEmgbPjY9VKwx3t0UfCO3Lp.jpg',content:$('.article-content').html(),title:$(".article-header").text(),big_img:'http://ac-a5zjlnxg.clouddn.com/9bVyIixddpREs3SgJcEmgbPjY9VKwx3t0UfCO3Lp.jpg',url_href:opts.url}, {
 							success: function(url_href) {
 								console.log("获取详情成功");	
 								// arr.push(url_href.attributes.url_href);
